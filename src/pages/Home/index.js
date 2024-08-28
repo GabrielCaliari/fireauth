a45c273422f0c4e7e74bcbc8725d6782cd54d66d
Gabriel Caliari <gabriel15caliari@hotmail.com>
2024-08-28 15:13:14 -0300

    Criando a pag Home e estilizando ela

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,48 @@
-
+import { useState } from "react"
+import "./home.css"
+import { Link } from "react-router-dom"
 
 export default function Home () {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleLogin(e){
+    e.preventDefault ()
+
+    if(email !== "" && password !== ""){
+      alert("Teste")
+    }
+    else{
+      alert("Preencha todos os campos!")
+    }
+  }
+
   return (
-    <div>
-      <h1>Pagina Home</h1>
+    <div className="home-container">
+      <h1>Lista de tarefas</h1>
+      <span>Gerencie sua agenda de forma fácil.</span>
+
+      <form className="form" onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Digite seu email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          autoComplete={false}
+          type="text"
+          placeholder="*******"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="Submit">Acessar</button>
+      </form>
+
+      <Link className="button-link" to="/register">
+        Não possui uma conta? Cadastre-se
+      </Link>
     </div>
   )
 }
