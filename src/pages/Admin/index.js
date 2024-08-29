07cb6649602baaee1bf9011f6f731306a41a7302
Gabriel Caliari <gabriel15caliari@hotmail.com>
2024-08-29 13:23:13 -0300

    criando e estilizando a pag admin

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,10 +1,43 @@
+import { useState } from "react"
 import "./admin.css"
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebaseConnection";
 
 
 export default function Admin() {
+  const [tarefa, setTarefa] = useState('')
+
+  function handleRegister(e){
+    e.preventDefault();
+  }
+
+  async function handleLogout(){
+    await signOut(auth);
+  }
+
   return (
-    <div>
-      Pagina ADMIN
+    <div className="admin-container">
+      <h1>Minhas tarefas</h1>
+
+      <form className="form" onSubmit={handleRegister}>
+        <textarea
+          placeholder="Digite sua tarefa..."
+          value={tarefa}
+          onChange={(e) => setTarefa(e.target.value)}
+        />
+
+        <button className="btn-register" type="submit">Registrar tarefa</button>
+      </form>
+
+      <article className="list">
+        <p>Estudar JavaScript</p>
+        <div>
+          <button>Editar</button>
+          <button className="btn-delete">Concluir</button>
+        </div>
+      </article>
+
+      <button className="btn-logout" onClick={handleLogout}>Sair</button>
     </div>
   )
 }
